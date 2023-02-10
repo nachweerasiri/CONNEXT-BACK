a0330b014b39c372dd5a601624d8cec5e463ad41
nachweerasiri <nach.weerasiri@gmail.com>
2023-02-10 21:41:47 +0700

    overwrite profile with cloudinary

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,9 +1,10 @@
-// const fs = require("fs");
+const fs = require("fs"); // library ที่เอาไว้ลบรูป
 // const { Op } = require("sequelize");
 // const createError = require("../utils/create-error");
 // const express = require("express");
 const { User } = require("../models");
 const createError = require("../utils/create-error");
+const cloudinary = require("../utils/cloudinary");
 
 exports.getUserInfoById = async (req, res, next) => {
     try {
@@ -26,9 +27,54 @@ exports.getUserInfoById = async (req, res, next) => {
 
 exports.updateProfileImage = async (req, res, next) => {
     try {
-        console.log(req.files);
-        res.status(200).json();
+        let value;
+        if (!req.files.profileImage && !req.files.posterImage) {
+            createError("profile image is require");
+        } else if (req.files.profileImage && req.files.posterImage) {
+            const [profileImage, posterImage] = await Promise.all([
+                cloudinary.upload(
+                    req.files.profileImage[0].path,
+                    req.user.profileImage
+                        ? cloudinary.getPublicId(req.user.profileImage)
+                        : null
+                ),
+                cloudinary.upload(
+                    req.files.profileImage[0].path,
+                    req.user.posterImage
+                        ? cloudinary.getPublicId(req.user.posterImage)
+                        : null
+                ),
+            ]);
+            value = { profileImage, posterImage };
+        } else if (req.files.profileImage) {
+            const profileImage = await cloudinary.upload(
+                req.files.profileImage[0].path,
+                req.user.profileImage
+                    ? cloudinary.getPublicId(req.user.profileImage)
+                    : null
+            );
+            value = { profileImage };
+        } else {
+            const posterImage = await cloudinary.upload(
+                req.files.posterImage[0].path,
+                req.user.posterImage
+                    ? cloudinary.getPublicId(req.user.posterImage)
+                    : null
+            );
+            value = { posterImage };
+        }
+
+        await User.update(value, { where: { id: req.user.id } });
+        res.status(200).json({ message: "success update" });
     } catch (err) {
         next(err);
+    } finally {
+        /** DELETE ลบรูป */
+        if (req.files.profileImage) {
+            fs.unlinkSync(req.files.profileImage[0].path);
+        }
+        if (req.files.posterImage) {
+            fs.unlinkSync(req.files.posterImage[0].path);
+        }
     }
 };
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,22 @@
+const cloudinary = require("../config/cloudinary");
+
+exports.upload = async (filePath, publicId) => {
+    const option = {
+        unique_filename: false,
+        use_filename: true,
+        overwrite: true,
+    };
+
+    if (publicId) {
+        option.public_id = publicId;
+    }
+
+    const result = await cloudinary.uploader.upload(filePath, option);
+    return result.secure_url;
+};
+
+/** ทำเพื่อลบรูปใน cloudinary ให้มัน overwrite ตรงรูปโปรไฟล์ // อัพใหม่ แล้วเก่าหาย */
+exports.getPublicId = url => {
+    const splitSlash = url.split("/");
+    return splitSlash[splitSlash.length - 1].split(".")[0];
+};
